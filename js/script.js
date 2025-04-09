2d951fe442d0ddce6fb767feaeafa83ba9d080a7
Tona Tony <140627191+TonaTony@users.noreply.github.com>
2025-04-09 15:34:36 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+//toggle navbar icon
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//Typing animation
+var typed = new Typed(".multiple-text", {
+   strings: ["Programmer", "Developer", "IT technician", "Entrepreneur"],
+   typeSpeed: 100,
+   backSpeed: 100,
+   backDelay: 1000,
+   loop: true 
+})
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+
+            //active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }  
+    });
+
+    //sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon and navbar when click(scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+//Dark mode
+function darkmode() {
+    const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+    localStorage.setItem('darkmode', !wasDarkmode);
+    const element = document.body;
+    element.classList.toggle('dark-mode', !wasDarkmode);
+}
+
+function onload() {
+    document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+}
