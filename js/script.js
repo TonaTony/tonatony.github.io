ccc9f8302e5ac7810b021bab298d504b93669757
Tona Tony <140627191+TonaTony@users.noreply.github.com>
2025-06-19 23:13:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ menuIcon.onclick = () => {
 
 //Typing animation
 var typed = new Typed(".multiple-text", {
-   strings: ["Programmer", "Developer", "IT technician", "Entrepreneur"],
+   strings: ["Programmer", "Developer", "IT technician"/*, "Entrepreneur"*/],
    typeSpeed: 100,
    backSpeed: 100,
    backDelay: 1000,
